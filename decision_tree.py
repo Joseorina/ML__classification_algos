a1193bd47bd8c7955dc409d86efeab66d41fc87c
Joseorina <orinangare@gmail.com>
2019-11-11 21:53:53 +0300

    Decision Tree Classifier

--- decision_tree.py
+++ decision_tree.py
@@ -1,2 +1,7 @@
-# -*- coding: utf-8 -*-
+#Decision Tree Classifier
+import pandas as pd
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]]
+y = dataset.iloc[:,4]
