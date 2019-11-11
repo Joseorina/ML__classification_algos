66f56913f8cb31c7884e2590336ff641b0a4a257
Joseorina <orinangare@gmail.com>
2019-11-11 21:10:41 +0300

    Naive Bayes Classifier

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,1 +1,8 @@
 #Naive Bayes
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]]
+y = dataset.iloc[:,4]
