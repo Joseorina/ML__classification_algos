6c6ce5ae103ae0f5718b338b36b272995e26a125
Joseorina <orinangare@gmail.com>
2019-11-12 07:20:42 +0300

    K-Means Clustering

--- clustering/k_means_clustering.py
+++ clustering/k_means_clustering.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:,[3,4]].values
+
+#using the elbow method to find optimal number of clusters
+from sklearn.cluster import KMeans
+wcss =[]
+for in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11), wcss)
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
