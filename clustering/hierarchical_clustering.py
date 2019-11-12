79c07fcd80b0acf75140669758c41a1372967c2a
Joseorina <orinangare@gmail.com>
2019-11-12 08:44:12 +0300

    Hierarchichal clustering

--- clustering/hierarchical_clustering.py
+++ clustering/hierarchical_clustering.py
@@ -4,3 +4,18 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:,[3,4]].values
+
+#Using the dendogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
+plt.show()
+
+#Fitting hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5,affinity='euclidean',linkage='ward')
+y_hc = hc.fit_predict(X)
+
+#Visualizing the cluster
