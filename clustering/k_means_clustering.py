955a9e5946f2579ec4458a8b35e4c25780438d67
Joseorina <orinangare@gmail.com>
2019-11-12 07:58:32 +0300

    K-Means Clustering

--- clustering/k_means_clustering.py
+++ clustering/k_means_clustering.py
@@ -8,7 +8,7 @@ X = dataset.iloc[:,[3,4]].values
 #using the elbow method to find optimal number of clusters
 from sklearn.cluster import KMeans
 wcss =[]
-for in range(1, 11):
+for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init='k-means++',max_iter=300,n_init=10,random_state=0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
@@ -17,4 +17,21 @@ plt.plot(range(1,11), wcss)
 plt.title('The elbow method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
+plt.show()
+
+#Apllying K-MEans to the mall data
+kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300,n_init=10, random_state=0)
+y_kmeans = kmeans.fit_predict(X)
+
+#Visualizing teh scatters
+plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 1], s=100,c='red',label='Careful')
+plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 1], s=100,c='blue',label='Standard')
+plt.scatter(X[y_kmeans==2, 0], X[y_kmeans==2, 1], s=100,c='green',label='Target')
+plt.scatter(X[y_kmeans==3, 0], X[y_kmeans==3, 1], s=100,c='cyan',label='careless')
+plt.scatter(X[y_kmeans==4, 0], X[y_kmeans==4, 1], s=100,c='magenta',label='Sensible')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='Centroids')
+plt.title('Clusters of clients')
+plt.xlabel('Annual income')
+plt.ylabel('Spending score')
+plt.legend()
 plt.show()
