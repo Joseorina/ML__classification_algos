5b0a4d86c4ea0903db237353f5d1bd6fbc16f2c7
Joseorina <orinangare@gmail.com>
2019-11-14 02:47:13 +0300

    ANN

--- deep_learning/ANN/artificial_neural_networks.py
+++ deep_learning/ANN/artificial_neural_networks.py
@@ -11,3 +11,11 @@ X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:,13].values
 
 #encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:,1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] =labelencoder_X_2.fit_transform(X[:,2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:,1:]
