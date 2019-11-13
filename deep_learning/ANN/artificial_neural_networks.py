3586c352646e72ef4b96109fb8ff09090ffb3a34
Joseorina <orinangare@gmail.com>
2019-11-14 02:50:53 +0300

    ANN

--- deep_learning/ANN/artificial_neural_networks.py
+++ deep_learning/ANN/artificial_neural_networks.py
@@ -23,3 +23,11 @@ X = X[:,1:]
 #spliting the dataest into training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+#feture scalling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Making the ANN
