ac454837a5e8175b72553d38eeb05dadf271ab2f
Joseorina <orinangare@gmail.com>
2019-11-14 02:40:53 +0300

    Artificial Neural Networks

--- deep_learning/ANN/artificial_neural_networks.py
+++ deep_learning/ANN/artificial_neural_networks.py
@@ -1,1 +1,13 @@
-import keras
+#Artificial Neural Network
+
+#data preprocessing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#import dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:,13].values
+
+#encoding categorical data
