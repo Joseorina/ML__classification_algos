68c4d549a27307f8dcc2e578fa6b6f8731539968
Joseorina <orinangare@gmail.com>
2019-11-14 03:07:59 +0300

    ANN

--- deep_learning/ANN/artificial_neural_networks.py
+++ deep_learning/ANN/artificial_neural_networks.py
@@ -1,55 +1,77 @@
-#Artificial Neural Network
+# Artificial Neural Network
 
-#data preprocessing
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-#import dataset
+# Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:,13].values
+y = dataset.iloc[:, 13].values
 
-#encoding categorical data
+# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:,1])
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
-X[:, 2] =labelencoder_X_2.fit_transform(X[:,2])
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
-X = X[:,1:]
+X = X[:, 1:]
 
-#spliting the dataest into training set and test set
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-#feture scalling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#Making the ANN(Part 2)
-#import keras libraries and packages
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-#initializing the ANN
+# Initialising the ANN
 classifier = Sequential()
 
-#adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init='uniform',activation='relu',input_dim=11))
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
 
-#adding the second hidden layer
+# Adding the second hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 
-#adding the output layer
-classifier.add(Dense(output_dim=1,init='uniform',activation='sigmoid'))
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 
-#compiling the ANN
-classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-#fitting ANN to the training set
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
